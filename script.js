65777afee04ee9b5b6ce9954034ef8e0f7fbbf22
Mike MacKenzie <mike_mack7@hotmail.com>
2020-08-14 12:21:47 -0400

    Proper file organization

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ function startVideo() {
 }
 
 video.addEventListener('play', () => {
-  // Initiate all face reading
   const canvas = faceapi.createCanvasFromMedia(video)
   overlay.append(canvas)
   const displaySize = { width: video.width, height: video.height }
@@ -26,29 +25,42 @@ video.addEventListener('play', () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    //faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    //faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    // Control the number of pizza slices
+    //faceapi.draw.drawDetections(canvas, resizedDetections) // Draw the bounding boxes of all detected faces
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections) // Draw the facial landmarks
+    //faceapi.draw.drawFaceExpressions(canvas, resizedDetections) // Draw the expressions value
     console.log(getFaceScore(detections));
     updatePizza(getFaceScore(detections));
   }, 100)
 })
 
 function updatePizza(score) {
-  if (score > 0.5) {
+  if (score > 0.75) {
     showPizza('pizza1')
     hidePizza('pizza2')
     hidePizza('pizza3')
     hidePizza('pizza4')
     hidePizza('pizza5')
   }
-  else if (score < -0.5) {
+  else if (score > 0.25 & score < 0.75){
+    showPizza('pizza1')
+    showPizza('pizza2')
+    hidePizza('pizza3')
+    hidePizza('pizza4')
+    hidePizza('pizza5')
+  }
+  else if (score < -0.75) {
     showPizza('pizza1')
     showPizza('pizza2')
     showPizza('pizza3')
     showPizza('pizza4')
     showPizza('pizza5')
+  }
+  else if (score > -0.75 & score < -0.25) {
+    showPizza('pizza1')
+    showPizza('pizza2')
+    showPizza('pizza3')
+    showPizza('pizza4')
+    hidePizza('pizza5')
   }
   else {
     showPizza('pizza1')
